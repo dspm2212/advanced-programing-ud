4d7ab11cdd2ea952427d954f7c9365fdbc799bbd
dspm2212 <dspm221204@gmail.com>
2024-06-05 23:31:25 -0500

    templates

--- advanced programming project/Codes/frondent/VirtualXperinces_app/urls.py
+++ advanced programming project/Codes/frondent/VirtualXperinces_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='xperiences_home'),
     path('login/', views.login, name="xperiences_login"),
+    path('register/', views.register, name="xperiences_register"),
+    path('dashboard/', views.dashboard, name="xperiences_dashboard"),
 ]   
--- advanced programming project/Codes/frondent/VirtualXperinces_app/views.py
+++ advanced programming project/Codes/frondent/VirtualXperinces_app/views.py
@@ -7,3 +7,9 @@ def home(request):
 
 def login(request): 
     return render(request,'login.html')
+
+def register(request): 
+    return render(request,'register.html')
+
+def dashboard(request): 
+    return render(request,'dashboard.html')
