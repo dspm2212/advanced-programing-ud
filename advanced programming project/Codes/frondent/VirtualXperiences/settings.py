0e904ff8c4d5689c2fe32c1ce1c94643b4721baf
dspm2212 <dspm221204@gmail.com>
2024-06-05 22:15:24 -0500

    fronend
    creacion del entorno para el frontend, actualizacion y correccion de los errores que se tenian, primera plantilla para el home

--- advanced programming project/Codes/frondent/VirtualXperiences/settings.py
+++ advanced programming project/Codes/frondent/VirtualXperiences/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'VirtualXperinces_app.apps.VirtualxperincesAppConfig',
+    'VirtualXperinces_app',
 ]
 
 MIDDLEWARE = [
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT= BASE_DIR/ "static"
+STATIC_ROOT= BASE_DIR / "static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- advanced programming project/Codes/frondent/VirtualXperiences/urls.py
+++ advanced programming project/Codes/frondent/VirtualXperiences/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("xperiences/",include("VirtualXperiences.urls"))
+    path("xperiences/",include("VirtualXperinces_app.urls"))
 ]
